9c1b1223478bf3f3d13fc5c749cf57bf07e5cb00
Logan Shire <logan.shire@gmail.com>
2017-07-21 02:19:02 -0700

    Models, service, handlers all working

--- Tests/SwaggerStencilTests/Templates/Go/Server/Packages/handlers/handlers.go
+++ Tests/SwaggerStencilTests/Templates/Go/Server/Packages/handlers/handlers.go
@@ -3,59 +3,103 @@ package handlers
 
 {% import "handler_parameters.stencil" %}
 {% import "handler_responses.stencil" %}
+{% import "item_conversion.stencil" %}
 {% import "query_conversion.stencil" %}
+{% import "path_conversion.stencil" %}
+{% import "header_conversion.stencil" %}
+{% import "response_names.stencil" %}
 import (
 	"encoding/json"
 	"net/http"
 
 	"github.com/attilathefun/registry"
-	"{{ path }}/models"
 	"{{ path }}/service"
+	"github.com/gorilla/mux"
 )
 
 {% for path,pathObject in swagger.paths %}
 {% for operationType,operation in pathObject.operations %}
 {% set handlerName %}{{ path|handlerName:operationType }}{% endset %}
-{% set comment %}{{ handlerName }}Handler - {{ operation.description }}{% endset %}
+{% set comment %}{{ handlerName }} - {{ operation.description }}{% endset %}
 {{ comment|wrapWidth:120,"// " }}
 func {{ handlerName }}(w http.ResponseWriter, r *http.Request) {
 {% set contents %}
+{% set hasQueryParameter %}{{ operation|hasParameter:"query" }}{% endset %}
+{% set hasPathParameter %}{{ operation|hasParameter:"path" }}{% endset %}
+{% set hasHeaderParameter %}{{ operation|hasParameter:"header" }}{% endset %}
 var err error
 maybeService, err := registry.Resolve((*service.Service)(nil))
 service := maybeService.(service.Service)
-{# Check if function has a query parameter: #}
-{% set hasQueryParameter %}{{ operation|hasParameter:"query" }}{% endset %}
-{% if hasQueryParameter %}
-queryParameters := r.URL.Query()
-{% endif %}
 
-{# Check if function has a path parameter: #}
-{# Parse out all the parameters into their respective types: #}
+{% if hasQueryParameter %}
+// Has query parameter(s):
+queryParameters := r.URL.Query()
+
+{% endif %}
+{% if hasPathParameter %}
+// Has path parameter(s):
+pathParameters := mux.Vars(r)
+
+{% endif %}
+{% if hasHeaderParameter %}
+// Has header parameter(s):
+headerParameters := r.Header
+
+{% endif %}
 {% for either in operation.parameters %}
 {% set parameterName %}{{ either|parameterName }}{% endset %}
 {% set isQuery %}{{ either|isParameter:"query" }}{% endset %}
 {% set isPath %}{{ either|isParameter:"path" }}{% endset %}
-{% set isHeader %}{{ either|isParameter:"path" }}{% endset %}
+{% set isHeader %}{{ either|isParameter:"header" }}{% endset %}
 {% set isBody %}{{ either|isParameter:"body" }}{% endset %}
+// Extract {{ parameterName|toCamel }}:
+
 {% if isBody %}
 BODY
-{% elif isPath %}
-PATH
-{% elif isHeader %}
-HEADER
 {% elif isQuery %}
-	{% call convertQueryParameter parameterName either %}
+{% call convertQueryParameter parameterName either %}
+{% elif isPath %}
+{% call convertPathParameter parameterName either %}
+{% elif isHeader %}
+{% call convertHeaderParameter parameterName either %}
 {% endif %}
 {% endfor %}
-{# Call the function with the parsed parameters: #}
-{# Return the appropriate result: #}
+// Call the function with the parsed parameters:
+{% call responseNames operation %} := service.{{ handlerName }}({% call handlerParameterNames operation %})
+
+// Marshal the response:
+var bytes []byte
+switch code {
+{% for code,either in operation.responses %}
+case {{ code }}:
+    bytes, err = json.Marshal(response{{ code }})
+{% endfor %}
+{% if operation.defaultResponse %}
+default:
+    bytes, err = json.Marshal(defaultResponse)
+{% endif %}
+}
+
+// Check if the marshaling was successful:
+if err != nil {
+    w.WriteHeader(500)
+    w.Write([]byte(err.Error()))
+    return
+}
+
+// Write the status code and the marshaled bytes:
+w.WriteHeader(code)
+w.Write(bytes)
 {% endset %}
 {{ contents|setIndentation:"    " }}
 }
 
 {% endfor %}
 {% endfor %}
-
+{% endimport %}
+{% endimport %}
+{% endimport %}
+{% endimport %}
 {% endimport %}
 {% endimport %}
 {% endimport %}
