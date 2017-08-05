e153f387ada6326fcc6a891e1835a5bd524a5545
Logan Shire <logan.shire@gmail.com>
2017-08-05 19:57:40 +0200

    Bump package version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "SwaggerStencil",
     dependencies: [
         .Package(url: "https://github.com/behrang/YamlSwift.git", majorVersion: 3),
-        .Package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", majorVersion: 1),
-        .Package(url: "https://www.github.com/AttilaTheFun/SwaggerParser.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/SwiftGen/StencilSwiftKit.git", majorVersion: 2),
+        .Package(url: "https://github.com/AttilaTheFun/SwaggerParser.git", majorVersion: 0, minor: 4)
     ]
 )
